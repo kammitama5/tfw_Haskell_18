5ab7307a06718424ed0821804c18e4c5cd1be037
kammitama5 <krystal.maughan@gmail.com>
2018-03-02 15:22:44 -0800

    Add files via upload

---
+++ simpl.hs
@@ -0,0 +1,26 @@
+-- 3.8
+
+-- 1a)
+concat [[1,2,,3], [4,5,6]]
+-- 1b)
+[1,2,3] ++ [4,5,6]
+-- 1c)
+(++) "hello" "world"
+-- 1d)
+["hello" ++ " world"]
+-- 1e)
+"hello" !! 4
+-- 1f)
+(!!) "hello" 4
+-- 1g) 
+take 4 "lovely"
+-- 1h)
+take 3 "awesome"
+
+-- 2
+-- a) d)
+-- b) c)
+-- c) e)
+-- d) a)
+-- e) b)
+
