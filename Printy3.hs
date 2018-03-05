d05346ba96de3349774c620db7109d50f30280c2
kammitama5 <krystal.maughan@gmail.com>
2018-03-05 09:39:39 -0800

    Add files via upload

---
+++ Printy3.hs
@@ -0,0 +1,7 @@
+-- string operated on is always same
+-- variable represents number of the letter you want to return
+
+module Printy2 where
+
+anyNum :: Int -> Char
+anyNum x = "Curry is awesome" !! x
