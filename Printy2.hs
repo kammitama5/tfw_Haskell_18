679b07388caaa9737c608e6e263f49cd7a01a615
kammitama5 <krystal.maughan@gmail.com>
2018-03-05 09:35:11 -0800

    Update Printy2.hs

--- Printy2.hs
+++ Printy2.hs
@@ -1,11 +1,3 @@
--- Write a function of type String -> Char 
--- which returns the third character of a string 
--- Remember to give the function a name 
--- and apply it to a variable, not a 
--- specific String, so that it 
--- could be reused for different String 
--- inputs, as demonstrated.
-
 -- thirdLetter :: String -> Char 
 -- thirdLetter x = x !! 2
 
