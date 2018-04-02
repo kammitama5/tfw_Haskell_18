409517b79393b7218bb6832c9b5e68b20f8d1cf4
kammitama5 <krystal.maughan@gmail.com>
2018-04-02 10:02:38 -0700

    another way of writing Datatypes

---
+++ Ha.hs
@@ -0,0 +1,9 @@
+module Ha where
+
+data Eng = Eng { developer :: String
+			   , salary :: Int
+			   , language :: String
+				} deriving (Show)
+				
+				
+-- let web_dev = Eng "burger flipper" 45000 "JavaScript"
