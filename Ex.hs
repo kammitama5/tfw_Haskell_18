1ea04add07908d5323706e88b1fd854bd03ea457
kammitama5 <krystal.maughan@gmail.com>
2018-04-05 10:48:33 -0700

    Add files via upload

---
+++ Ex.hs
@@ -0,0 +1,7 @@
+-- Programming in Haskell
+
+-- write an expression that checks whether a list is empty.
+let isempty x = if null(x) then True else False
+
+-- write an expression that checks whether a list has one element.
+let hasone x = if length(x) == 1 then True else False
