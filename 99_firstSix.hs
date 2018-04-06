2d921b0b2c3f702f3a9702dad329d05b9600b6e0
kammitama5 <krystal.maughan@gmail.com>
2018-04-06 16:16:49 -0700

    Add files via upload

---
+++ 99_firstSix.hs
@@ -0,0 +1,26 @@
+-- 99 problems haskell
+
+-- find the last element of a list 
+
+module Ac where
+
+import Data.List
+
+-- 1. Find the last element of a list
+-- let myLast x = last x
+
+-- 2. Find the last but one element of a list
+-- let myButLast x = head $ reverse $ init x
+
+-- 3. Find the kth element of a list
+-- let elementAt x y = last $ take y x
+
+-- 4. Find the number of elements of a list
+-- let myLength x = length x
+
+-- 5. Reverse a list
+-- let myReverse x = reverse x
+
+-- 6. Find out whether a list is a palindrome.
+-- let isPalindrome x = if x `compare` reverse x == EQ then True else False
+
