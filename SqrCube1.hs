d23545ce63408bdec510e0573d38f9a7f28d992c
kammitama5 <krystal.maughan@gmail.com>
2018-04-20 11:38:34 -0700

    Update SqrCube1.hs

--- SqrCube1.hs
+++ SqrCube1.hs
@@ -4,4 +4,5 @@ let myCube1 = filter (> 50) [y^3 | y <- [1..5]]
 let mySqr1 = filter (< 50) [x^2 | x <- [1..5]]
 let myTups1 = zip mySqr1 myCube1
 
--- outputs [64,125]
+-- outputs [64,125], [(4, 125)]
+-- let lentupboth1 = length (zip mySqr1 myCube1) => 2
